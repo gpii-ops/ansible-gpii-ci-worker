6a696083b4f4d954bd9d182185a827f9575be083
Tyler Roscoe <tyler@cryptio.net>
2017-06-05 18:45:03 -0600

    Pull in ansible-gitlab-runner.
    Not sure if we'll need this long-term but it's convenient to work with this role under molecule.

--- tests/test_default.py
+++ tests/test_default.py
@@ -5,5 +5,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_something():
-    pass
+def test_gitlab_service_enabled_and_running(Service):
+    svc = Service("gitlab-runner")
+    assert svc.is_enabled
+    assert svc.is_running
